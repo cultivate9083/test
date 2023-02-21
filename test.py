a7252c3be47d5a04e74ba806142bb947882114fd
s1nack <s1nack@protonmail.com>
2023-02-21 01:52:54 +0000

    test

---
+++ test.py
@@ -0,0 +1,18 @@
+# retrieve environment variables
+
+import os
+
+print(os.environ['HOME'])
+
+# create function
+
+def test():
+    print('This is a test function')
+
+# call function
+
+test()
+
+my_tests = [1, 2, 3, 4, 5]
+for my_test in my_tests:
+    print(my_test)
